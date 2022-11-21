4890e38b75ec1c02e5092b9acfa9f2e11d08baff
PIYoung <dlsdudg15@naver.com>
2022-11-21 10:01:24 +0900

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,15 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
+      host: "127.0.0.1",
+      port: 7545,
       network_id: "*", // Match any network id
       gas: 5000000,
     },
   },
   compilers: {
     solc: {
+      version: "^0.8.0",
       settings: {
         optimizer: {
           enabled: true, // Default: false
