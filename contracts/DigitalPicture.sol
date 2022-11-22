06fc5378f06933ed15d140b7a02f476e23ab3126
PIYoung <dlsdudg15@naver.com>
2022-11-23 05:07:06 +0900

    update

--- contracts/DigitalPicture.sol
+++ contracts/DigitalPicture.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.17 < 0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract DigitalPicture is ERC721URIStorage {
   using Counters for Counters.Counter;
@@ -10,11 +10,15 @@ contract DigitalPicture is ERC721URIStorage {
 
   constructor() ERC721("DigitalPicture", "NDP") {}
 
-  // web3.js
+  // public으로 정의된 함수만 web3.js에서 쓸 수 있다.
   function publishItem(address owner, string memory tokenURI) public returns(uint256) {
-    _tokenIds.increment();
+    _tokenIds.increment(); // begin 1
 
-    uint256 newItemId = _tokenIds.current();
-    _mint(owner, newItemId);
+    uint256 newTokenId = _tokenIds.current();
+    // ERC 721 앞에 언더바는 internal 함수
+    _mint(owner, newTokenId);
+    _setTokenURI(newTokenId, tokenURI);
+
+    return newTokenId;
   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const DigitalPicture = artifacts.require("DigitalPicture");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+module.exports = function (deployer) {
+  deployer.deploy(DigitalPicture);
 };
